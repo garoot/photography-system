8a8825406b7b25a4e2a87c98ac1d062a1d44ef93
Abdulmajeed <majeed.garoot@gmail.com>
2023-11-13 23:56:43 +1100

    grid initiad fade-in is working

--- frontend/src/components/PhotoGrid/PhotoGrid.js
+++ frontend/src/components/PhotoGrid/PhotoGrid.js
@@ -1,15 +1,75 @@
 import React from 'react';
-import './PhotoGrid.css'; // Make sure the path is correct for your project structure
+import { useEffect, useLayoutEffect, useRef  } from 'react';
+import './PhotoGrid.css';
 
-function PhotoGrid({ photos }) { // Assume photos is passed as a prop
+function PhotoGrid({ photos }) {
+  // Determine the number of columns based on the window width or a fixed number
+    const numColumns = 6; // Or calculate based on window width
+    const columns = Array.from({ length: numColumns }, () => []);
+
+    // Distribute photos into columns
+    photos.forEach((photo, index) => {
+    columns[index % numColumns].push(photo);
+    });
+
+    // useEffect(() => {
+    //     // Set a timeout to add the fade-in class
+    //     const timer = setTimeout(() => {
+    //         const photoItems = document.querySelectorAll('.photo-item');
+    //         photoItems.forEach(item => {
+    //             item.classList.add('fade-in');
+    //         });
+    //     }, 100); // Adjust this delay as needed
+
+    //     // Cleanup the timeout if the component unmounts
+    //     return () => clearTimeout(timer);
+    // }, []);
+
+    const gridRef = useRef(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            console.log('Scroll event triggered'); // Add this line
+            const { current } = gridRef;
+            if (current) {
+                const columnElements = current.getElementsByClassName('photo-column');
+                for (let i = 0; i < columnElements.length; i++) {
+                const offset = (window.scrollY / 10) * (i % 2 === 0 ? -1 : 1);
+                columnElements[i].style.transform = `translateY(${offset}px)`;
+                }
+            }
+        };
+        console.log('Adding scroll event listener'); // Add this line
+
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+            console.log('Removing scroll event listener'); // Add this line
+
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+        
+
     return (
-        <div className="photo-grid">
-            {photos.map((photo, index) => (
-                <div key={index} className="photo-item">
-                    <img src={`http://localhost:4000/${photo.url}`} alt={photo.title || `Photography ${index}`} />
-                </div>
+    <div ref={gridRef} className="photo-grid">
+        {columns.map((column, columnIndex) => (
+        <div
+            key={columnIndex}
+            className={`photo-column ${columnIndex % 2 === 0 ? 'moveUp' : 'moveDown'}`}
+        >
+            {column.map((photo, photoIndex) => (
+            <div key={photoIndex} className="photo-item">
+                <img 
+                    src={`http://localhost:4000/${photo.url}`} 
+                    alt={photo.title || `Photography ${photoIndex}`} 
+                    loading="eager"
+                />
+            </div>
             ))}
         </div>
+        ))}
+    </div>
     );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Import createRoot
 import './index.css'; // Global styles
 import App from './App'; // The root component for your application
 
-ReactDOM.render(
+// Find the root element in your HTML
+const rootElement = document.getElementById('root');
+// Create a root using createRoot
+const root = createRoot(rootElement);
+
+// Use the root.render method to render your app component
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
