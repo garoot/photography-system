e7f84534202b9b9208c4c4f7dd1f3b471edf94d6
Abdulmajeed <majeed.garoot@gmail.com>
2023-11-15 04:53:57 +1100

    added the ease-in for ServicesSection

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,23 +2,30 @@ import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 
 function Navbar() {
-    const [backgroundColor, setBackgroundColor] = useState('rgba(7, 13, 22, 0)'); // Initial color is fully transparent
+    const [backgroundColor, setBackgroundColor] = useState('rgba(7, 13, 22, 0)');
+    const [isLoaded, setIsLoaded] = useState(false); // State to handle the fade-in
 
     useEffect(() => {
         const handleScroll = () => {
-            const newColor = window.scrollY > 0 ? 'rgba(7, 13, 22, 1)' : 'rgba(7, 13, 22, 0)'; // Solid color when scrolled
+            const newColor = window.scrollY > 0 ? 'rgba(7, 13, 22, 0.94)' : 'rgba(7, 13, 22, 0)';
             setBackgroundColor(newColor);
         };
 
         window.addEventListener('scroll', handleScroll);
 
+        // Set a timeout to delay the fade-in effect
+        const timer = setTimeout(() => {
+            setIsLoaded(true);
+        }, 500); // Delay in milliseconds
+
         return () => {
             window.removeEventListener('scroll', handleScroll);
+            clearTimeout(timer); // Clear the timeout if the component unmounts
         };
     }, []);
 
     return (
-        <nav className="navbar" style={{ backgroundColor: backgroundColor }}>
+        <nav className={`navbar ${isLoaded ? 'fade-in' : ''}`} style={{ backgroundColor: backgroundColor }}>
             <div className="navbar-content">
                 <div className="navbar-logo">
                     <img src="/logo-white@1.5x.png" alt="Malak Photo" />
--- frontend/src/components/ServicesSection/ServicesSection.js
+++ frontend/src/components/ServicesSection/ServicesSection.js
@@ -1,11 +1,43 @@
-// ServicesSection.js
-import React from 'react';
-import styles from './ServicesSection.module.css'; // Path to your CSS module
+import React, { useEffect, useRef } from 'react';
+import styles from './ServicesSection.module.css';
 
 function ServicesSection() {
+    const refServices = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        const serviceItems = refServices.current.children;
+                        for (let i = 0; i < serviceItems.length; i++) {
+                            setTimeout(() => {
+                                serviceItems[i].classList.add(styles.serviceItemVisible);
+                            }, i * 100);
+                        }
+                    }
+                });
+            },
+            { threshold: 0.1 }
+        );
+
+        if (refServices.current) {
+            observer.observe(refServices.current);
+        }
+
+        return () => {
+            if (refServices.current) {
+                observer.unobserve(refServices.current);
+            }
+        };
+    }, []);
+
     return (
-        <div className={styles.services}>
-            {/* Icons and descriptions for services go here */}
+        <div className={styles.services} ref={refServices}>
+            {/* Render your service items here */}
+            <div className={`${styles.serviceItem}`}>Service 1</div>
+            <div className={`${styles.serviceItem}`}>Service 2</div>
+            {/* ... other items */}
         </div>
     );
 }
