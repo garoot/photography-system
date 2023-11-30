aa9b16719c41aebc0b690ccb30836d690adcd41b
Abdulmajeed <majeed.garoot@gmail.com>
2023-11-30 23:53:51 +1100

    added some aesthetics to the PortfolioVideos component

--- frontend/src/components/Dashboard/PortfolioVideos/PortfolioVideos.js
+++ frontend/src/components/Dashboard/PortfolioVideos/PortfolioVideos.js
@@ -22,11 +22,37 @@ const PortfolioVideos = () => {
         };
         fetchVideos();
     }, []); // Empty dependency array ensures this effect runs once on mount
-    
-    return (
-        <>
 
-        </>
+    const handleFormSubmit = async (event, videoId) => {
+        event.preventDefault();
+        // Add your form submission logic here
+    };
+
+    
+return (
+        <div className="portfolio-videos-container">
+            {videos.map(video => (
+                <form key={video._id} onSubmit={(e) => handleFormSubmit(e, video._id)} className="video-form">
+                    <input type="hidden" name="_id" value={video._id} />
+
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" id="title" name="title" defaultValue={video.title} required />
+
+                    <label htmlFor="videoUrl">Video URL:</label>
+                    <input type="text" id="videoUrl" name="videoUrl" defaultValue={video.videoUrl} required />
+
+                    <label htmlFor="thumbnailUrl">Current Thumbnail:</label>
+                    <img src={video.thumbnailUrl} alt="Thumbnail" />
+                    <label htmlFor="newThumbnail">Upload New Thumbnail:</label>
+                    <input type="file" id="newThumbnail" name="thumbnailUrl" accept="image/*" />
+
+                    <label htmlFor="description">Description:</label>
+                    <textarea id="description" name="description" defaultValue={video.description} required />
+
+                    <button type="submit">Update Video</button>
+                </form>
+            ))}
+        </div>
     );
 };
 
